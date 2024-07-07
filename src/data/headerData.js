66a3a66c46392289a06d28130e131bf03aa31931
Tulsipada <tulsipada55@gmail.com>
2024-07-07 08:29:01 +0530

    setup

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -6,7 +6,7 @@ export const headerData = {
   name: "Tulsipada Das",
   title: "Sr. NodeJs Developer",
   desciption:
-    "There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
+    "The sky was empty, but that wasn’t what captured his attention. It was the clouds. Their deep green hue was unnatural, unsettling. He knew what was coming and hoped he was ready. ",
   image: profileImg,
   resumePdf: resume,
 };
