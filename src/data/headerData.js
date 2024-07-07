ee6ed5cead9984772114aeea579295db78a0c9c0
Tulsipada <tulsipada55@gmail.com>
2024-07-07 13:47:35 +0530

    setup

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,9 +4,8 @@ import profileImg from "../assets/IMG/Tulsipada.avif";
 
 export const headerData = {
   name: "Tulsipada Das",
-  title: "Sr. NodeJs Developer",
-  desciption:
-    "The sky was empty, but that wasn’t what captured his attention. It was the clouds. Their deep green hue was unnatural, unsettling. He knew what was coming and hoped he was ready. ",
+  title: "Jr. NodeJs Developer",
+  desciption: `The sky was empty, but that wasn’t what captured his attention. It was the clouds. Their deep green hue was unnatural, unsettling. He knew what was coming and hoped he was ready.`,
   image: profileImg,
   resumePdf: resume,
 };
