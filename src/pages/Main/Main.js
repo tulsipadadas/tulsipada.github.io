40e0373493ad75a26371a6a50bc07044d35f0761
Tulsipada <tulsipada55@gmail.com>
2024-07-26 22:32:13 +0530

    fixed Accessibility

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 
-import { Navbar, Footer, Landing, Blog, About, Skills, Education, Experience, Contacts, Projects, Services, Testimonials } from '../../components'
+import { Navbar, Footer, Landing, About, Skills, Education, Experience, Contacts, Services, Testimonials } from '../../components'
 import { headerData } from '../../data/headerData'
 
 function Main() {
